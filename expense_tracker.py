86547208811db0267859a2d9844dac825f0d8aed
Atila438 <104580581+Atila438@users.noreply.github.com>
2025-09-01 21:37:25 +0300

    Third commit

--- expense_tracker.py
+++ expense_tracker.py
@@ -18,3 +18,24 @@ def save_expenses(expenses):
 
 def generate_id(expenses):
     return max([exp["id"] for exp in expenses], default=0) + 1
+# ---------------- Commands ----------------
+def add_expense(description, amount, category, currency):
+    expenses = load_expenses()
+    expense = {
+        "id": generate_id(expenses),
+        "date": datetime.date.today().isoformat(),
+        "description": description,
+        "amount": amount,
+        "category": category,
+        "currency": currency.upper()  # Store in uppercase for consistency
+    }
+    expenses.append(expense)
+    save_expenses(expenses)
+    print(f"Expense added successfully (ID: {expense['id']})")
+
+def list_expenses():
+    expenses = load_expenses()
+    print("ID  Date       Description       Amount   Category   Currency")
+    for exp in expenses:
+        print(f"{exp['id']}   {exp['date']}  {exp['description']}  "
+              f"{exp['amount']} {exp['currency']}   {exp['category']}")

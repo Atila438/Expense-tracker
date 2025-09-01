6bb2881ef3f168a17796a749e843d42916071127
Atila438 <104580581+Atila438@users.noreply.github.com>
2025-09-01 21:34:43 +0300

    Second commit

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,4 +1,20 @@
 import argparse
 import json
 import os
-from datetime import datetime
+import datetime
+
+DATA_FILE = "expenses.json"
+
+# ---------------- Helper Functions ----------------
+def load_expenses():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
+    return []
+
+def save_expenses(expenses):
+    with open(DATA_FILE, "w") as f:
+        json.dump(expenses, f, indent=4)
+
+def generate_id(expenses):
+    return max([exp["id"] for exp in expenses], default=0) + 1
